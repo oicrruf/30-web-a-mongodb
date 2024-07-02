afda2cf72da83a3a85126457139ecf3ae85335e9
oicrruf <vm.reyesal@gmail.com>
2024-07-01 20:45:26 -0600

    chore: product routes

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
 require("dotenv").config();
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const productRouter = require("./routes/product");
 
 app = express();
 
@@ -14,7 +16,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+mongoose
+  .connect(process.env.DATABASE_URI)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log(err));
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/product", productRouter);
 
 module.exports = app;
---
+++ routes/product.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+// Todos los productos
+router.get("/", (req, res, next) => {
+  res.send({ message: "Soy product GET" });
+});
+
+// Productos por ID
+router.get("/:id", (req, res, next) => {
+  res.send({ message: req.params.id });
+});
+
+// Crear producto
+router.post("/", (req, res, next) => {
+  res.send({ message: "Soy product GET" });
+});
+
+// Actualizar producto
+router.patch("/:id", (req, res, next) => {
+  res.send({ message: req.params.id });
+});
+
+// Eliminar producto
+router.delete("/:id", (req, res, next) => {
+  res.send({ message: req.params.id });
+});
+
+module.exports = router;
